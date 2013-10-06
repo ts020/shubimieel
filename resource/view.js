bdd572d36b91118442f2db120e09e38954bf1965
Keisuke OSONE <paradi.bass@gmail.com>
2013-10-06 12:03:54 +0900

    Draw Circle at Random with position color

--- resource/view.js
+++ resource/view.js
@@ -17,7 +17,14 @@ var MainView = (function (_super) {
         */
         //console.log("Hello World!");
         var canvas  = document.getElementById("mainCanvas").getContext("2d");
-        drawCircle(canvas, x, y);
+
+        for (var i=0; i < 27; i++) {
+            position = parseInt(Math.random() * 10);
+            x = Math.random() * 640;
+            y = Math.random() * 960;
+            drawCircle(canvas, x, y, position);
+        }
+
     };
 
     MainView.prototype.viewComplete = function () {
@@ -31,19 +38,46 @@ var MainView = (function (_super) {
     以下は描画のための関数
 
 */
-
 //円を描画
-function drawCircle(canvas, x, y) {
+function drawCircle(canvas, x, y, positionNumber) {
     console.log("Draw!")
     canvas.beginPath();
         canvas.lineWidth = 1;
-        canvas.strokeStyle = "#ff0000";
+        //canvas.strokeStyle = "#ff0000";
 
         var sAng = 0;            //円弧の開始角度
         var eAng = 2 * Math.PI;  //円弧の終端角度
-        canvas.arc(x, y, 30, sAng, eAng, true);
+        canvas.arc(x, y, 10, sAng, eAng, true);
         canvas.stroke();
 
-        canvas.fillStyle = "#ff0000";
+        canvas.fillStyle = fillPositionColor(positionNumber);
         canvas.fill();
 }
+
+function fillPositionColor(positionNumber) {
+    switch (positionNumber){
+        case 1:
+            return "#ff0000";
+        case 2:
+            return "0000ff";
+        case 3:
+            return "ffff00";
+        case 4:
+            return "ffff00";
+        case 5:
+            return "ffff00";
+        case 6:
+            return "ffff00";
+        case 7:
+            return "00bb00";
+        case 8:
+            return "00bb00";
+        case 9:
+            return "00bb00";
+        default:
+            return "ffffff";
+
+    }
+}
+
+
