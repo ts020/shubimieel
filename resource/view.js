2b279ca4bf6c11aa6961ace043bf524a462402de
Keisuke OSONE <paradi.bass@gmail.com>
2013-10-06 11:09:32 +0900

    Viewで円を描画

--- resource/view.js
+++ resource/view.js
@@ -10,6 +10,14 @@ var MainView = (function (_super) {
     };
 
     MainView.prototype.draw = function () {
+        /*
+        document.body.addEventListener("click", function(){
+            alert("click");
+        });
+        */
+        //console.log("Hello World!");
+        var canvas  = document.getElementById("mainCanvas").getContext("2d");
+        drawCircle(canvas, x, y);
     };
 
     MainView.prototype.viewComplete = function () {
@@ -17,3 +25,25 @@ var MainView = (function (_super) {
     };
     return MainView;
 })(events.EventDispatcher);
+
+/*
+
+    以下は描画のための関数
+
+*/
+
+//円を描画
+function drawCircle(canvas, x, y) {
+    console.log("Draw!")
+    canvas.beginPath();
+        canvas.lineWidth = 1;
+        canvas.strokeStyle = "#ff0000";
+
+        var sAng = 0;            //円弧の開始角度
+        var eAng = 2 * Math.PI;  //円弧の終端角度
+        canvas.arc(x, y, 30, sAng, eAng, true);
+        canvas.stroke();
+
+        canvas.fillStyle = "#ff0000";
+        canvas.fill();
+}
